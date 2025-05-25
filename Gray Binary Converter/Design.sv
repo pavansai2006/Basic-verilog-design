a5ea52ad9aa6d629e950bf934d5e6871ffe0f1ad
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-25 22:10:17 +0530

    Create Design.sv

---
+++ Gray Binary Converter/Design.sv
@@ -0,0 +1,8 @@
+module gray_binary_converter(bin,gray);
+  input [3:0] gray;
+  output [3:0] bin;
+  assign bin[3] = gray[3];
+  assign bin[2] = gray[3]^gray[2];
+  assign bin[1] = gray[2]^gray[1];
+  assign bin[0] = gray[1]^gray[0];
+endmodule
