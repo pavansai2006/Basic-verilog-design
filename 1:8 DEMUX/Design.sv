2ec26bac5f6751552d471404cacd5614ec462491
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-25 15:42:54 +0530

    Create Design.sv

---
+++ 1:8 DEMUX/Design.sv
@@ -0,0 +1,20 @@
+module demux1to8(in,en,sel,out);
+  input in,en;
+  input [2:0] sel;
+  output reg [7:0] out;
+  always @(*)
+    begin
+      out=8'b00000000;
+      case(sel)
+        3'b000: out[0] = in;
+        3'b001: out[1] = in;
+        3'b010: out[2] = in;
+        3'b011: out[3] = in;
+        3'b100: out[4] = in;
+        3'b101: out[5] = in;
+        3'b110: out[6] = in;
+        3'b111: out[7] = in;
+        default: out = 8'b00000000;
+      endcase
+    end
+endmodule
