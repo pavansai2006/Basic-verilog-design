d0a98adf965273d59236b49e4860dc124774c527
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-25 20:52:48 +0530

    Create Design.sv

---
+++ Octal Binary Encoder/Design.sv
@@ -0,0 +1,7 @@
+module octal_binary_encoder(in,out);
+  input [7:0] in;
+  output [2:0] out;
+  assign out[2] = in[4]|in[5]|in[6]|in[7];
+  assign out[1] = in[2]|in[3]|in[6]|in[7];
+  assign out[0] = in[1]|in[3]|in[5]|in[7];
+endmodule
