e2c4c736b0ff0f90a94210513232460f66390617
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-27 15:33:29 +0530

    Create Testbench.sv

---
+++ PIPO/Testbench.sv
@@ -0,0 +1,27 @@
+module pipo_tb();
+  wire [3:0] pout;
+  reg clk,rst,ld;
+  reg [3:0] pin;
+  pipo PIPO(clk,rst,ld,pin,sout);
+  initial
+    begin
+      $dumpfile("pipo.vcd");
+      $dumpvars(0,pipo_tb);
+    end
+  initial
+    begin
+      clk=0;
+      forever #5 clk=~clk;
+    end
+  initial
+    begin
+      rst=1; ld=0; pin=4'b1101; #5
+      rst=0; ld=1; pin=4'b1010; #10
+      rst=1; pin=4'b0101; #10
+      pin=4'b1011; #10
+      pin=4'b1101;ld=0; #10
+      pin=4'b0000; #10
+      pin=4'b1101;ld=1; #5
+      $finish;
+    end
+endmodule
