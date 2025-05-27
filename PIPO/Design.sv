5f50c3e2a42ff181a8b770870f0a818f5b14cf6e
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-27 15:33:04 +0530

    Create Design.sv

---
+++ PIPO/Design.sv
@@ -0,0 +1,15 @@
+module pipo(clk,rst,ld,pin,pout);
+  input clk,rst,ld;
+  input [3:0] pin;
+  output sout;
+  output reg [3:0] pout;
+  always @(posedge clk)
+    begin
+      if(!rst)
+      	pout=4'b0000;
+      else if(ld)
+        pout<=pin;
+      else
+        pout<=(pout<<1);
+    end
+endmodule
